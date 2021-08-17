e97dd1df645af82ee5eea80c1fa61d89d7c0f790
Golden Kalu <goldenlemmy@gmail.com>
2021-08-17 14:01:48 -0700

    put shopping cart

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,28 @@ router.post('/:userId/shoppingcart/:productId', async (req, res) => {
     }
 });
 
+router.put('/:userId/shoppingcart/:productId', async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error);
 
+        const user = await User.findById(req.params.userId);
+        if (!user) return res.status(400).send(`The user with id "${req.params.userId}" does not exist.`);
+
+        const product = user.shoppingCart.id(req.params.productId);
+        if (!product) return res.status(400).send(`The product with id "${req.params.productId}" does not in the users shopping cart.`);
+        product.name = req.body.name;
+        product.description = req.body.description;
+        product.category = req.body.category;
+        product.price = req.body.price;
+        product.dateModified = Date.now();
+
+        await user.save();
+        return res.send(product);
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
 
     
    
