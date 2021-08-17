9af91d9482def545f9ad083480de093c5dfd8239
Golden Kalu <goldenlemmy@gmail.com>
2021-08-17 00:16:12 -0700

    product route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const connectDB = require('./startup/db');
 const app = express();
 const cors = require('cors');
 const collections = require("./routes/collections");
+const products = require("./routes/products");
 const auth = require("./routes/auth");
 
 
@@ -12,9 +13,10 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use("/api/collections", collections);
+app.use("/api/products", products);
 app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
-})
+});
---
+++ routes/products.js
@@ -0,0 +1,30 @@
+const Product = require("../models/products");
+const express = require("express");
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try {
+        const product = new Product ({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price,
+      });
+
+      await product.save();
+ 
+      return res.send(product);
+    } catch (ex) {
+      return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+
+
+
+
+
+
+
+
+module.exports = router;
