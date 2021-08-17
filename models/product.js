1be328a3f45f82099a12edd19801a4960bb78632
Golden Kalu <goldenlemmy@gmail.com>
2021-08-17 11:50:18 -0700

    endpoint for all products

--- models/product.js
+++ models/product.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 const joi = require('joi');
 
-const productsSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
     name: { type: String, required: true, minlength: 5, maxlength: 200 },
     description: { type: String, required: true },
     category: { type: String, required: true, minlength: 5, maxlength: 50 },
@@ -10,16 +10,16 @@ const productsSchema = new mongoose.Schema({
     dateModified: { type: Date, default: Date.now },
 });
 
-const Products = mongoose.model('Products', productsSchema);
+const Product = mongoose.model('Product', productSchema);
 
-function validateProducts(products) {
+function validateProduct(product) {
     const schema = joi.object({
         name: Joi.string().min(2).max(50).required(),
         description: Joi.string().required(),
         category: Joi.string().min(5).max(50).required(),
         price: Joi.number().required(),
     });
-    return schema.validate(products);
+    return schema.validate(product);
 }
 
 
@@ -28,6 +28,6 @@ function validateProducts(products) {
 
 
 
-exports.Products = Products;
-exports.validate = validateProducts;
-exports.productsSchema = productsSchema;
+exports.Product = Product;
+exports.validate = validateProduct;
+exports.produtSchema = productSchema;
--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,28 @@
-const { Products, validate } = require("../models/products");
+const { Product, validate } = require("../models/product");
 const express = require("express");
 const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find();
+        return res.send(products);
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const product = await Product.findById(req.params.id);
+//         if (!product)
+//             return res.status(400).send(`The product with id "${req.params.id}" does not exist.`);
+
+//         return res.send(product);
+//     }   catch (ex) {
+//         return res.status(500).send(`Internal Server Error: ${ex}`);
+//     }
+// });
 
 
 
@@ -12,16 +34,16 @@ router.post('/', async (req, res) => {
         if (error)
             return res.status(400).send(error);
 
-        const products = new Products ({
+        const product = new Product ({
             name: req.body.name,
             description: req.body.description,
             category: req.body.category,
             price: req.body.price,
         });
 
-      await products.save();
+      await product.save();
  
-      return res.send(products);
+      return res.send(product);
     } catch (ex) {
       return res.status(500).send(`Internal Server Error: ${ex}`);
     }
